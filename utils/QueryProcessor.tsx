1cbcc8f371fd3364faba22652354a696a64f06ef
mwguo15 <mwguo@andrew.cmu.edu>
2024-03-14 16:26:59 -0400

    Added sqrt/cbrt, adding, and max queries

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -19,5 +19,35 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if (query.includes("Which of the following numbers is the largest: ")) {
+    let numbers = query.match(/[0-9]+/g);
+    let arr = numbers!;
+    const mapped = arr.map((x) => parseInt(x));
+    const max = Math.max(...mapped);
+    return max.toString();
+  }
+
+  if (query.includes("plus")) {
+    let numbers = query.match(/[0-9]+/g);
+    let arr = numbers!;
+    let sum = arr[0] + arr[1];
+    return sum.toString();
+  }
+
+  if (query.includes("square and a cube")) {
+    let numbers = query.match(/[0-9]+/g);
+    let arr = numbers!;
+    var isSquare = function (n : string) {
+      let number = Number(n);
+      return number > 0 && Math.sqrt(number) % 1 === 0;
+    };
+    var isCbrt = function (n : string) {
+      let number = Number(n);
+      return number > 0 && Math.cbrt(number) % 1 === 0;
+    };
+    const filtered = arr.filter((number) => isCbrt (number) && isSquare (number))
+    return filtered.toString();
+  }
+
   return "";
 }
